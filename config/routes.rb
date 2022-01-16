948e925118c63b17294ce0b3c5b7c0902b5dbff3
Keshav <keshavodedra90@gmail.com>
2022-01-16 12:43:09 +0400

    routing fixed for 404 urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   resources :media_files
+  
   devise_for :users
+  
   devise_scope :user do
     root :to => 'devise/sessions#new'
   end
+  
   get 'user', to: 'users#index'
-  # get '*path', to: redirect("/users/sign_up")
+
+  get '*all', to: redirect("/users/sign_up"), constraints: lambda { |req|
+    req.path.exclude? 'rails/active_storage'
+  }
 end
